d95551c785986f6410060bea25fa915d7f2ebf06
sarbesh <sarbeshsarkarsks@gmail.com>
2021-10-15 21:43:56 +0530

    Added core dependency

--- src/main/java/com/sarbesh/configserver/ConfigServerApplication.java
+++ src/main/java/com/sarbesh/configserver/ConfigServerApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableConfigServer
 @EnableEurekaClient
+@ComponentScan(basePackages = {"com.sarbesh.configserver","com.sarbesh.core"})
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
