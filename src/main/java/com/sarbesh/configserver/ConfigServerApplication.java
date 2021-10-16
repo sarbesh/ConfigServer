00a552b6bd27da78a48592ac5da4f98efb1b0a1a
sarbesh <sarbeshsarkarsks@gmail.com>
2021-10-16 09:59:10 +0530

    Added prod-security

--- src/main/java/com/sarbesh/configserver/ConfigServerApplication.java
+++ src/main/java/com/sarbesh/configserver/ConfigServerApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 @EnableConfigServer
@@ -14,6 +17,16 @@ public class ConfigServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+	@EnableWebSecurity
+	static class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.csrf().ignoringAntMatchers("/encrypt","/decrypt");
+			super.configure(http);
+		}
 	}
 
 }
